3cba1376381f48f75e9a7439e03b0d25cf77ae87
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-05-13 09:25:17 +0530

    feat: Add startup event to initialize AI system for render deployment

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from semantic_rag import initialize_system
 
 app = FastAPI()
 
+# =========================================================
+# STARTUP
+# =========================================================
+@app.on_event("startup")
+async def startup_event():
+
+    print("Initializing AI system...")
+
+    initialize_system()
+
+    print("AI system ready.")
+
+# =========================================================
+# ROOT
+# =========================================================
 @app.get("/")
 def home():
-    return {"status": "working"}
+
+    return {
+        "status": "working"
+    }
