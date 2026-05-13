d8fb57e946467c277db42b6bb168cb546b56065b
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-05-13 13:23:14 +0530

    Delete download_model.py

--- download_model.py
+++
@@ -1,9 +0,0 @@
-import os
-from sentence_transformers import SentenceTransformer
-
-# Force Hugging Face to save the model inside the project directory
-os.environ['HF_HOME'] = './hf_cache'
-
-print("Downloading model to local folder...")
-SentenceTransformer('all-MiniLM-L6-v2')
-print("Model cached locally successfully!")
